46f6be29e6bf8f86a94aa69428b5e3dd0b5c12d1
Clémentine Scohy <clembytes@proton.me>
2025-08-14 19:58:12 +0200

    SERVER: add css template in static folder

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::sync::{Arc, Mutex};
 
-use axum::{
-    extract::State, routing::get, Router
-};
+use axum::{Router, extract::State, routing::get};
+use tower_http::services::ServeDir;
 
 #[derive(Clone)]
 struct Coordinates {
@@ -11,19 +10,20 @@ struct Coordinates {
 
 #[tokio::main]
 async fn main() {
-    let coords = Coordinates {
+    let rustbot_coordinates = Coordinates {
         coordinates: Arc::new(Mutex::new((0, 0))),
     };
 
     // Build app with different routes
     let app = Router::new()
         .route("/", get(root))
-        .route("/reset", get(reset))
-        .route("/right", get(right))
-        .route("/left", get(left))
-        .route("/down", get(down))
-        .route("/up", get(up))
-        .with_state(coords);
+        .route("/reset", get(reset).post(reset))
+        .route("/right", get(right).post(right))
+        .route("/left", get(left).post(left))
+        .route("/down", get(down).post(down))
+        .route("/up", get(up).post(up))
+        .nest_service("static", ServeDir::new("rustbot/static"))
+        .with_state(rustbot_coordinates);
 
     // run app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
