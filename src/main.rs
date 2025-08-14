5b23e4f43f9f4a01100bd85fe3623e36e8018f78
Clémentine Scohy <clembytes@proton.me>
2025-08-14 15:14:54 +0200

    SERVER: hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    // build application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
